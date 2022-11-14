f31e047e22ac7843f7806cdc8a165ffa0af4bf4b
Swayam Prakash <swayam265@gmail.com>
2022-11-14 23:13:57 +0530

    [Swayam] : added functionality to perform update and delete operations in employee payroll application

--- EmployeePayroll/src/app/app.component.ts
+++ EmployeePayroll/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmployeePayroll';
+  constructor(private router: Router) {
+
+  }
+  goHome() {
+    this.router.navigate([""])
+  }
 
 }
--- EmployeePayroll/src/components/home/home.component.ts
+++ EmployeePayroll/src/components/home/home.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Employee1 } from 'src/app/model/employeeModel';
+
 import { EmployeeServiceService } from 'src/app/services/employee-service.service';
 
+@Injectable({
+  providedIn: 'root'
+})
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,14 +16,38 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private service: EmployeeServiceService
+    private service: EmployeeServiceService,
+
   ) { }
+
   list: any;
+  count: Number = 0;
+  public employeeUpdate: Employee1 = new Employee1("", 0, "", new Date, "", "", [])
+
   ngOnInit(): void {
+
     this.service.getEmployee().subscribe((data: any) => {
       console.log(data);
       this.list = data.data;
+      this.count = this.list.length;
     });
+
   }
+  onDelete(id: Number) {
+    console.log(id);
+    this.service.deleteEmployeeById(id).subscribe((data: any) => {
+      console.log(data);
+      this.ngOnInit();
+    })
+  }
+  id: Number = 0;
+
+  onUpdate(employeeId: Number, employeeUp: Employee1) {
+    console.log(employeeId, employeeUp)
+    this.service.updateDataFunc(employeeId, employeeUp);
+    this.router.navigate(["form"]);
+
+  }
+
 
 }
